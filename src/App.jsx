f607b17284e1038a7382dd0537636f3b2cbc0653
dolartand <dolartand@gmail.com>
2026-01-30 10:55:26 +0300

    feat: correct App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route} from "react-router-dom";
+import Layout from "./components/layout/Layout.jsx";
+import PrivateRoute from "./components/common/PrivateRoute.jsx";
+import AdminRoute from "./components/common/AdminRoute.jsx";
+import HomePage from "./components/pages/HomePage.jsx";
+import AdminDashboard from "./components/pages/AdminDashboard.jsx";
+import CreateOrderPage from "./components/pages/CreateOrderPage.jsx";
+import LoginPage from "./components/pages/LoginPage.jsx";
+import OrderDetailsPage from "./components/pages/OrderDetailsPage.jsx";
+import OrdersPage from "./components/pages/OrdersPage.jsx";
+import PaymentsPage from "./components/pages/PaymentsPage.jsx"
+import ProfilePage from "./components/pages/ProfilePage.jsx";
+import RegisterPage from "./components/pages/RegisterPage.jsx";
+import UsersPage from "./components/pages/UsersPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <Layout>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/register' element={<RegisterPage />} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <Route path="/profile" element={
+                    <PrivateRoute>
+                        <ProfilePage />
+                    </PrivateRoute>
+                } />
+                <Route path="/orders" element={
+                    <PrivateRoute>
+                        <OrdersPage />
+                    </PrivateRoute>
+                } />
+                <Route path="/orders/create" element={
+                    <PrivateRoute>
+                        <CreateOrderPage />
+                    </PrivateRoute>
+                } />
+                <Route path="/orders/:id" element={
+                    <PrivateRoute>
+                        <OrderDetailsPage />
+                    </PrivateRoute>
+                } />
+                <Route path="/payments" element={
+                    <PrivateRoute>
+                        <PaymentsPage />
+                    </PrivateRoute>
+                } />
+                <Route path="/admin" element={
+                    <AdminRoute>
+                        <AdminDashboard />
+                    </AdminRoute>
+                } />
+                <Route path="/admin/users" element={
+                    <AdminRoute>
+                        <UsersPage />
+                    </AdminRoute>
+                } />
+            </Routes>
+        </Layout>
+    );
 }
 
-export default App
+export default App;
